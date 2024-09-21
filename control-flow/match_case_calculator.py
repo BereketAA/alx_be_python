f056da05201dd995d3d56d5d7e26df11237b02e2
bereketa <beckyamare1@gmail.com>
2024-09-21 03:26:50 +0300

    make commit  match_case_calculator.py here

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+operation = input("chose the operation? (+,-,/,*): ")
+#def opertaion(value):
+match operation:
+        case '-':
+            result = f"The answer is = {num1 - num2}"
+        case '+':
+            result = f"The answer is = {num1 + num2}"
+        case '*':
+           result = f"The answer is = {num1 * num2}"
+        case '/':
+            if num2 != 0:
+               result = f"The answer is = {num1 / num2}"
+            else:
+                print( "Error: Cannot divide by zero")
+        case '_':
+           result = "Invalid operation selected."
+print(result)
