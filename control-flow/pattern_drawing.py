73c5895802098c746bba4dc2f1d74918fac2411d
bereketa <beckyamare1@gmail.com>
2024-09-21 23:49:33 +0300

    make commit pattern_drawing.py here

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,7 @@
 size = int(input("Enter the size of the  pattern: "))
 # Ensure the user entered a positive integer
 while size <= 0:
-    #print("Enter the size of the  pattern.")
+    print("Enter the size of the  pattern.")
     size = int(input("Enter the size of the  pattern: "))
 
 # Use nested loops to print the square pattern
