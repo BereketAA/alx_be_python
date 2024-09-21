91a3de895cbc8f1b2cb4d6167b8a3dc151479ef3
bereketa <beckyamare1@gmail.com>
2024-09-22 02:26:44 +0300

    make commit daily_reminder.py here

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,27 +7,31 @@ priority = input("Priority (high/medium/low):").lower()
 # Ask if the task is time-bound (yes or no)
 time_bound = input("Is it time-bound? (yes/no):").lower()
 
+#initialize reminder
+if time_bound == "yes":
+    reminder = f"Reminder: '{task}' is a "
+else:
+    reminder = f"Note:'{task}' is a "
 # Use Match Case to react based on task priority
 match priority:
     case 'high':
-        priority_message = "a high priority task"
+        reminder += "high priority task"
         #reminder_label = "Reminder"
     case 'medium':
-        priority_message = "a medium priority task"
+        reminder += "medium priority task"
         #reminder_label = "Reminder"
     case 'low':
-        priority_message = "a low priority task"
+        reminder += "low priority task"
         #reminder_label = "Note"
     case _:
-        priority_message = "an invalid priority task"
+        reminder += "invalid priority task"
 
 # Modify the reminder if the task is time-bound
-if time_bound == "yes" or priority == "high":
-    print(f"\nReminder: '{task}' is {priority_message} that requires immediate attention today!")
+if time_bound == "yes":
+   reminder += "that requires immediate attention today!"
 else:
-    print(f"\nNote: '{task}' is {priority_message}. Consider completing it when you have free time.")
+    reminder += ". Consider completing it when you have free time."
 
     
-
 # Print the final reminder
-#print(f"\n{reminder_label}:'{task}' is {priority_message} {time_message}")
+print(reminder)
