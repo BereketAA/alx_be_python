ca8c3bc17154ac349d506a0e5c3cfbca4fa0f3b1
bereketa <beckyamare1@gmail.com>
2024-09-27 02:18:54 +0300

    Make temp_conversion_tool.py executable

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,6 @@
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  # Factor for converting Fahrenheit to Celsius
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5   # Factor for converting Celsius to Fahrenheit
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9  # Factor for converting Fahrenheit to Celsius
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5   # Factor for converting Celsius to Fahrenheit
 FAHRENHEIT_OFFSET = 32                  # Offset for Fahrenheit conversion
 
 def convert_to_celsius(fahrenheit):
