94303dcc7d2cb1ef470d12b8433683bfebb871dd
bereketa <beckyamare1@gmail.com>
2024-10-06 01:09:34 +0300

    bank account here

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,17 +7,14 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount
-            #print(f"Deposited: ${amount}")
         else:
             print("Deposit amount must be positive.")
 
     def withdraw(self, amount):
         if 0 < amount <= self.account_balance:
             self.account_balance -= amount
-            #print(f"Withdrew: ${amount}")
             return True
         elif amount > self.account_balance:
-            #print("Insufficient funds.")
             return False
         else:
             print("Withdrawal amount must be positive.")
@@ -25,3 +22,5 @@ class BankAccount:
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
+
+
